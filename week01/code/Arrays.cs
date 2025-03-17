505df7d00879787a01fea689b87960a16d19159e
sara latorre <saralatorre97@gmail.com>
2025-03-18 00:35:58 +0100

    completed arrays functions

--- week01/code/Arrays.cs
+++ week01/code/Arrays.cs
@@ -13,7 +13,14 @@ public static class Arrays
         // step by step before you write the code. The plan should be clear enough that it could
         // be implemented by another person.
 
-        return []; // replace this return statement with your own
+        var numbers = new List<double>(length); // creates the list and sets its capacity to length
+
+        for(int i = 1; i <= length; i++) // loops form 1 to lenght and increments i while its smaller or equal to lenght
+        {
+            numbers.Add(number * i); // multiplicates the nb by the loop index and adds the result to the list
+        }
+
+        return numbers.ToArray(); // returns the list as an array
     }
 
     /// <summary>
@@ -29,5 +36,13 @@ public static class Arrays
         // Remember: Using comments in your program, write down your process for solving this problem
         // step by step before you write the code. The plan should be clear enough that it could
         // be implemented by another person.
+
+        int capacity = data.Count; // gets the total capacity of the list
+        int index = capacity - amount;  // calaculates the starting index for extracting the amout of numbers
+
+        var numbers = data.GetRange(index, amount); // stores the numbers from the starting index until the last number of the list into a variable 
+        data.RemoveRange(index, amount); // removes them from the back
+        data.InsertRange(0, numbers); // adds them at the front
+
     }
 }
